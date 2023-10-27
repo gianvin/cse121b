45811ae8e2949da962f432d447590692f8aac180
gianvin <ginamangahassanchezong@gmail.com>
2023-10-27 13:59:12 +0800

    assignment complete

--- w01-task.js
+++ w01-task.js
@@ -14,8 +14,8 @@ let theTotal = total(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 console.log(`The total is ${theTotal}`);
 
 // 'total' function declaration
-function total(...theNumbers) {
-	let sum = 0;
+function total(...theNumbers) {theNumbers = Array(10)
+	let sum = 0; sum = 0
 	for (let aNumber in theNumbers) {
 		sum += aNumber * 1; // Why do we use * 1? It implicitly converts a string to a number.
 	}
