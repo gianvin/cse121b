35edf5b98727092b1d04d58ece0708f22fce2f2a
gianvin <ginamangahassanchezong@gmail.com>
2023-10-31 15:13:44 +0800

    finished step 3

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -3,16 +3,19 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-const fullName = "Gina Ong";
+{const fullName = "Gina Ong";
 const currentYear = newDate().getFullYear();
 const profilePicture = "images/Gina Ong Photo.jpg";
-
+}
 
 
 
 /* Step 3 - Element Variables */
-
-
+{const nameElement = document.getElementById("name");
+ const foodElement = document.getElementById("food");
+ const yearElement = document.querySelector("#year");
+ const imageElement = document.getElementById("Gina Ong Photo,jpg");
+}
 
 
 
