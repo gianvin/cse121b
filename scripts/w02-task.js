590137fd4c61fb36517312f60857562f34952b46
gianvin <ginamangahassanchezong@gmail.com>
2023-11-02 13:46:45 +0800

    removed the braces

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -3,37 +3,36 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-{const fullName = "Gina Ong";
+ const fullName = "Gina Ong";
  const currentYear = new Date().getFullYear();
  const profilePicture = "images/Gina Ong Photo.jpg";
-}
+
 
 
 
 /* Step 3 - Element Variables */
-{const nameElement = document.getElementById("name");
+ const nameElement = document.getElementById("name");
  const foodElement = document.getElementById("food");
  const yearElement = document.querySelector("#year");
  const imageElement = document.getElementById("Gina Ong Photo,jpg");
-}
+
 
 
 
 /* Step 4 - Adding Content */
 
-{nameElement.innerHTML = `<string>${fullName}</string>`;
+ nameElement.innerHTML = `<string>${fullName}</string>`;
  yearElement.textContent = currentYear;
  imageElement.setAttribute("src" , profilePicture);
  const altText = `Profile image of ${fullName}`;
  imageElement.setAttribute("alt", altText);
-}
+
 
 
 
 
 /* Step 5 - Array */
-{const favoriteFoods = ["Carbonara", "Kare-kare", "Adobo", "Sizzling Steak", "Fresh Lumpia", "Ramen"];
- const foodElement = document.getElementById("food");
+ const favoriteFoods = ["Carbonara", "Kare-kare", "Adobo", "Sizzling Steak", "Fresh Lumpia", "Ramen"];
  const foodList = favoriteFoods.join(",");
  foodElement.textContent = `${foodList}`;
  const anotherFood = "Pansit";
@@ -43,7 +42,7 @@
  foodElement.innerHTML += `<br>${favoriteFoods}`;
  favoriteFoods.pop();
  foodElement.innerHTML += `<br>${favoriteFoods}`;
-}
+ 
 
 
 
