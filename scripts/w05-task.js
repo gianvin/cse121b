a0fac22ec2e5ed7edb334ad93c3df20c17421268
gianvin <ginamangahassanchezong@gmail.com>
2023-11-30 20:16:12 +0800

    edited display temples and get temples

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -21,25 +21,20 @@ templesElement.appendChild(articleElement);
 
 
 /* async getTemples Function using fetch()*/
+displayTemples();
+const locationSelect = document.getElementById('locationSelect');
+const notUtahOption = document.createElement('option');
+notUtahOption.value = "notutah";
+notUtahOption.textContent = "Outside of Utah";
+locationSelect.appendChild(notUtahOption);
+console.log(JSON.stringify(templeList)); 
+
 const getTemples = async () => {
-    try{
         const response = await fetch("https://byui-cse.github.io/cse121b-ww-course/resources/temples.json");
-       if (!response.ok){
-        throw new Error('HTTP error! Status: ${response.status}');
-       }
        const templesData = await response.json();
        templeList = templesData;
-       displayTemples();
-       const locationSelect = document.getElementById('locationSelect');
-       const notUtahOption = document.createElement('option');
-       notUtahOption.value = "notutah";
-       notUtahOption.textContent = "Outside of Utah";
-       locationSelect.appendChild(notUtahOption);
-       console.log(JSON.stringify(templeList));   
-}catch (error){
-    console.error('Error fetching temples', error.message);
-}
 };
+        
 
 
 /* reset Function */
