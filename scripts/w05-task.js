b530c59bd84f6e0f9ff3fc699e7452aa97898f10
gianvin <ginamangahassanchezong@gmail.com>
2023-12-01 19:40:06 +0800

    edited the sortBy

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -2,46 +2,45 @@
 
 /* Declare and initialize global variables */
 const templesElement = document.getElementById("temples");
-const templeList = [];
+
 
 /* async displayTemples Function */
+let templeList = [];
 const displayTemples = (filteredTemples) => {
     reset();
-    const templesToDisplay = filteredTemples || templeList;
+   const templesToDisplay = filteredTemples || templeList;
    templesToDisplay.forEach(function(temple){
-    const articleElement = document.createElement('article');
-const h3Element = document.createElement('h3');
-h3Element.textContent = temple.templeName;
-const imgElement = document.createElement('img');
-imgElement.src = temple.imageUrl;
-imgElement.alt = temple.location;
-templesElement.appendChild(h3);
-templesElement.appendChild(img);
-templesElement.appendChild(article);
+   const articleElement = document.createElement('article');
+   const h3Element = document.createElement('h3');
+   h3Element.textContent = temple.templeName;
+   const imgElement = document.createElement('img');
+   imgElement.src = temple.imageUrl;
+   imgElement.alt = temple.location;
+   articleElement.appendChild(h3Element);
+   articleElement.appendChild(imgElement);
+   templesElement.appendChild(articleElement);
 });
 };
 
 
 /* async getTemples Function using fetch()*/
-const getTemples = async () => {
-    try{
-        const response = await fetch("https://byui-cse.github.io/cse121b-ww-course/resources/temples.json");
+async function getTemples(){
+    const response = await fetch("https://byui-cse.github.io/cse121b-ww-course/resources/temples.json");
+    const data = await response.json();
+    templeList = data;
 
-       const templesData = await response.json();
-       templeList = templesData;
-       displayTemples();
-       const locationSelect = document.getElementById('locationSelect');
-       const notUtahOption = document.createElement('option');
-       notUtahOption.value = "notutah";
-       notUtahOption.textContent = "Outside of Utah";
-       locationSelect.appendChild(notUtahOption);
-       console.log(JSON.stringify(templeList));   
-}catch (error){
-    console.error('Error fetching temples', error.message);
+    const templeDiv = document.getElementById("temples")
+    templeList.forEach(temple => {
+        const article = document.createElement("article");
+        article.textContent = temple.templeName;
+        templeDiv.appendChild(article);
+
+        const img = document.createElement("img")
+        img.setAttribute("src", temple.imgageUrl);
+        templeDiv.appendChild(img);
+    });
+
 }
-};
-
-
 /* reset Function */
 const reset = function(){
     const articleElements = templesElement.querySelectorAll('article');
@@ -49,7 +48,6 @@ const reset = function(){
         article.remove();
     })
 }
-reset();
 /* sortBy Function */
 const sortBy = () =>{
     reset();
@@ -57,23 +55,20 @@ const sortBy = () =>{
     
     switch (filter){
         case "utah":
-            displayTemples(Utah);
+        displayTemples(templeList.filter (temple => temple.location === "Utah"));
             break;
-        case "nonutah":
-            displayTemples();
+        case "notutah":
+            displayTemples(templeList.filter(temple => temple.location !== "Utah"));
             break;
         case "older":
-            displayTemples(new Date(1950, 0, 1));
-            break;
+            displayTemples(templeList.filter(temple => new Date(temple.constructionDate) < new Date(1950, 0, 1)));
+        break;
         case "all":
-            displayTemples(temples);
+            displayTemples(templeList);
             break;
     }
-    reset();
-    displayTemples();
+    
 };
-
-   reset();
 
 /* Event Listener */
 document.querySelector("#sortBy").addEventListener("change", () => {sortBy(templeList)});
