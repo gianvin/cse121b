54aa8f6b92683efcd8d61cbae2bcdb168c2233a9
gianvin <ginamangahassanchezong@gmail.com>
2023-12-08 11:54:19 +0800

    renamed district4.jpg

--- scripts/district.js
+++ scripts/district.js
@@ -1,24 +1,28 @@
 /*variables*/
 const districtName = "Malabon IV Elementary Schools";
+const currentYear = new Date().getFullYear();
 const districtLogo = "images/distict4.jpg";
 
 /*element variables*/
 const nameElement = document.getElementById("name");
 const schoolElement = document.getElementById("school");
+const  yearElement = document.querySelector("#year");
 const logoElement = document.getElementById("img");
 
 /*adding Content*/
 nameElement.innerHTML = `<string>${districtName}<string>`;
+yearElement.textContent = currentYear;
 logoElement.setAttribute('src', districtLogo);
 const altText = `District Logo  of ${districtName}`;
 logoElement.setAttribute("alt", altText);
 
 /*Array*/
-const schools = ["GuillermoS. Sanchez Memorial Elementary School", "Potrero Elementary School I", "Bagong Lote Elementary School", "Tinajeros Elementary School"];
+const schools = ["Guillermo S. Sanchez Memorial Elementary School", "Potrero Elementary School I", "Bagong Lote Elementary School", "Tinajeros Elementary School"];
 const schoolList = schools.join(",");
 schoolElement.textContent = `${schoolList}`;
 const anotherSchool = "Potrero Elementary School Main";
 schools.push(anotherSchool);
+schools.shift();
 schoolElement.innerHTML += `<br>${schools}`;
 schoolElement.innerHTML += `<br>${schools}`;
 schools.pop();
