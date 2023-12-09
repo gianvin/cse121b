8ae3b3d5c1b420666d0882ee95e7889628120f4b
gianvin <ginamangahassanchezong@gmail.com>
2023-12-09 19:06:11 +0800

    edited the data.js

--- scripts/data.js
+++ scripts/data.js
@@ -8,8 +8,7 @@ document.querySelector("#addNumbers1").addEventListener('click', function (){
     let addNumber1 = Number(document.querySelector('#add1_school1').value);
     let addNumber2 = Number(document.querySelector('#add2_school1').value);
     document.querySelector('#sum1').value = add(addNumber1, addNumber2);})
- /*Update District Totals*/
- updateDistrictTotal();
+
 
 
     /*function for school2*/
@@ -21,8 +20,6 @@ document.querySelector("#addNumbers2").addEventListener('click', function (){
     let addNumber2 = Number(document.querySelector('#add2_school2').value);
     document.querySelector('#sum2').value = add(addNumber1, addNumber2);})
 
-/*Update District Totals*/
-updateDistrictTotal();
 
     /*function for school3*/
 function add (number1, number2){
@@ -33,8 +30,7 @@ document.querySelector("#addNumbers3").addEventListener('click', function (){
     let addNumber2 = Number(document.querySelector('#add2_school3').value);
     document.querySelector('#sum3').value = add(addNumber1, addNumber2);})
 
-/*Update District Totals*/
-updateDistrictTotal();
+
 
     /*function for school4*/
 function add (number1, number2){
@@ -45,8 +41,6 @@ document.querySelector("#addNumbers4").addEventListener('click', function (){
     let addNumber2 = Number(document.querySelector('#add2_school4').value);
     document.querySelector('#sum4').value = add(addNumber1, addNumber2);})
 
-    /*Update District Totals*/
- updateDistrictTotal();
 
     /*function for school5*/
 function add (number1, number2){
@@ -57,33 +51,37 @@ document.querySelector("#addNumbers5").addEventListener('click', function (){
     let addNumber2 = Number(document.querySelector('#add2_school5').value);
     document.querySelector('#sum5').value = add(addNumber1, addNumber2);})
  
-/*Update District Totals*/
- updateDistrictTotal();
-
  
-/*function to update district totals*/
-function updateDistrictTotal() {
-    const districtMaleTotal = calculateTotal("input[name='add1']");
-    const districtFemaleTotal = calculateTotal("input[name='add2']");
-    const districtTotal = districtMaleTotal + districtFemaleTotal;
-
-    document.getElementById("districtMaleTotal").value = districtMaleTotal;
-    document.getElementById("districtFemaleTotal").value = districtFemaleTotal;
-    document.getElementById("districtTotal").value = districtTotal;
+/*function for district totals*/
+function add (number1, number2, number3, number4, number5){
+    return number1 + number2 + number3 + number4 + number5;
 }
-/* Function to calculate total based on input name*/
-function calculateTotal(inputName){
-    const inputs = document.querySelectorAll(inputName);
-    let total = 0;
+document.querySelector("#addNumbers6").addEventListener('click'), function (){
+    let addNumber1 = Number(document.querySelector('#add1_school1').value);
+    let addNumber2 = Number(document.querySelector('#add1_school2').value);
+    let addNumber3 = Number(document.querySelector('#add1_school3').value);
+    let addNumber4 = Number(document.querySelector('#add1_school4').value);
+    let addNumber5 = Number(document.querySelector('#add1_school5').value);
+    document.querySelector('#districtMaleTotal').value = add(addNumber1, addNumber2, addNumber3, addNumber4, addNumber5)};
 
-    inputs.forEach(function (input){
-        total += parseFloat(input.value) || 0;
-    });
-
-    return total;
-}
-
-
+function add (number1, number2, number3, number4, number5){
+        return number1 + number2 + number3 + number4 + number5;
+    }
+    document.querySelector("#addNumber7").addEventListener('click'), function (){
+        let addNumber1 = Number(document.querySelector('#add2_school1').value);
+        let addNumber2 = Number(document.querySelector('#add2_school2').value);
+        let addNumber3 = Number(document.querySelector('#add2_school3').value);
+        let addNumber4 = Number(document.querySelector('#add2_school4').value);
+        let addNumber5 = Number(document.querySelector('#add2_school5').value);
+        document.querySelector('#districtFemaleTotal').value = add(addNumber1, addNumber2, addNumber3, addNumber4, addNumber5)};
+       
+        function add (number1, number2){
+            return number1 + number2 ;
+        }
+        document.querySelector("#addNumber8").addEventListener('click'), function (){
+            let addNumber1 = Number(document.querySelector('#districtMaleTotal').value);
+            let addNumber2 = Number(document.querySelector('#districtFemaleTotal').value);
+            document.querySelector('#districtTotal').value = add(addNumber1, addNumber2)};
 
        
     
